a6c9b4366dbe415d2b09b0644b9bce56c0ece344
TJ <thaddeusjiang@gmail.com>
2025-11-07 21:20:56 +0900

    mise, input-source-pro

--- setup.sh
+++ setup.sh
@@ -6,30 +6,19 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
 # Install Homebrew packages
-brew install git
-brew install elixir
-brew install gh
 brew install trash
 brew install tree
 brew install fzf
-brew install babashka/brew/bbin
-brew install postgresql
-brew install flyctl
 brew install thefuck
-brew install MonitorControl
+
+# Development tools
+brew install git
+brew install mise
+brew install gh
+brew install jq
 
 # Install Homebrew casks
-brew install --cask lastpass
-brew install --cask arc
-brew install --cask alfred
-brew install --cask rectangle
-brew install --cask visual-studio-code
 brew install --cask fork
-brew install --cask eudic
-brew install --cask docker
-brew install --cask postman
-
+brew install --cask input-source-pro
+brew install --cask monitorcontrol
